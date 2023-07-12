c6bff687e78c20e7e439cf10073151fc12166995
Emanuel Grigolatto <ema_grigo@hotmail.com>
2023-07-12 18:07:55 -0300

    primero

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+import { Boton } from './componentes/Boton'
+import { BotonClear } from './componentes/BotonClear'
+import { Pantalla } from './componentes/Pantalla'
+import { evaluate} from 'mathjs'
+import './App.css'
+
+function App() {
+  
+  const [input, setInput] = useState('');
+
+  const agregarInput = (val) => {
+      setInput(input + val);
+  };
+  
+  const calcularResultado = () => {
+    if (input) {
+      setInput(evaluate(input));
+    } else {
+      alert('Ingrese un numero gil')
+    }  
+  };
+
+  return (
+    <div className='App'>
+      <div className="contenedor-calculadora">
+        <Pantalla input={input}/>
+        <div className="fila">
+          <Boton manejarClic={agregarInput}>1</Boton>
+          <Boton manejarClic={agregarInput}>2</Boton>
+          <Boton manejarClic={agregarInput}>3</Boton>
+          <Boton manejarClic={agregarInput}>+</Boton>
+        </div>
+        <div className="fila">
+          <Boton manejarClic={agregarInput}>4</Boton>
+          <Boton manejarClic={agregarInput}>5</Boton>
+          <Boton manejarClic={agregarInput}>6</Boton>
+          <Boton manejarClic={agregarInput}>-</Boton>
+        </div>
+        <div className="fila">
+          <Boton manejarClic={agregarInput}>7</Boton>
+          <Boton manejarClic={agregarInput}>8</Boton>
+          <Boton manejarClic={agregarInput}>9</Boton>
+          <Boton manejarClic={agregarInput}>*</Boton>
+        </div>
+        <div className="fila">
+          <Boton manejarClic={calcularResultado}>=</Boton>
+          <Boton manejarClic={agregarInput}>0</Boton>
+          <Boton manejarClic={agregarInput}>.</Boton>
+          <Boton manejarClic={agregarInput}>/</Boton>
+        </div>
+        <div className="fila">
+          <BotonClear manejarClear={() => setInput('')}>Clear</BotonClear>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/componentes/Pantalla.jsx
@@ -0,0 +1,11 @@
+import '../estilos/Pantalla.css'
+
+const Pantalla = ({ input }) => {
+    return(
+        <div className='input'>
+            {input}
+        </div>
+    )
+}
+
+export { Pantalla }
